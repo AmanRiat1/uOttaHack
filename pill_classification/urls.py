61ac7cee3894106aed39944a77ea84176115d560
Athavan1919 <ajeev047@uottawa.ca>
2020-02-07 21:20:52 -0500

    Another test

--- pill_classification/urls.py
+++ pill_classification/urls.py
@@ -13,14 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
+
 from . import views
 
-urlpattern = [path('', views.index, name = 'index')]
-
-
 urlpatterns = [
-    path('pill_classification/', include('pill_classification.urls')),
-    path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
 ]
