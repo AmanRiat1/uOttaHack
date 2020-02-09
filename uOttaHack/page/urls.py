4e7db2eee8a6874e18f3391f356d80070fb634bf
Athavan1919 <ajeev047@uottawa.ca>
2020-02-08 21:36:49 -0500

    idk

--- uOttaHack/page/urls.py
+++ uOttaHack/page/urls.py
@@ -1,9 +1,10 @@
 from django.urls import include, path
-from . import views
+
+from .views import index, about, test, question_create_view
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('/about', views.about, name='index'),
-    path('test/', views.test)
-
+    path('', index, name='index'),
+    path('/about', about, name='index'),
+    path('test/', test),
+    path('create/', question_create_view)
 ]
