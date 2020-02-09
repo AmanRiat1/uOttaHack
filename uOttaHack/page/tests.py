7fe95aa77540e4b27d32d3afe4b59d29f7db57a9
Tony Guan <tonyguan123@hotmail.com>
2020-02-08 19:44:22 -0500

    as

--- uOttaHack/page/tests.py
+++ uOttaHack/page/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
 # Create your tests here.
+def index(request):
+    return render(request, 'page/home.html')
+
+def about(request):
+    return render(request, 'page/About.html')
+    
+def test(request):
+    return render(request, 'page/test.html')
