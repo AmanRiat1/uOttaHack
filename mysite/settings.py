255c5c4dbcf6d8630e2997705735afd9a42e0b30
Aman Riat <amanriat777@gmail.com>
2020-02-15 00:04:01 -0500

    added styling

--- mysite/settings.py
+++ mysite/settings.py
@@ -101,4 +101,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
--- mysite/urls.py
+++ mysite/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 from . import views
 
 
 urlpatterns = [
     url(r'^$', views.basic, name='basic'),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
